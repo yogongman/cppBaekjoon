d525ce3d7d80dfa9d7e8672e1e44ca725efd0c2d
dlekgns6743 <dlekgns6743@naver.com>
2021-02-15 15:55:21 +0900

    str

---
+++ basic/2675.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+    int n;
+    int m[1000];
+    string a[1000];
+    cin >> n;
+    for(int i =0; i<n; i++){
+        cin>>m[i]>>a[i];
+    }
+    for(int i=0; i<n; i++){
+        for(int j=0; j<a[i].size(); j++){
+            for(int k=0; k<m[i]; k++){
+                cout<<a[i][j];
+            }
+        }
+        cout<<"\n";
+    }
+    return 0;
+    
+}
