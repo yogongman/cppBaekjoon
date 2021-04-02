161d7f973b4a88296703b5fb5ef87d2707733d75
dlekgns6743 <dlekgns6743@naver.com>
2021-04-02 14:49:06 +0900

    .

---
+++ class_3/7576.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    int lo[1001][1001];
+    cin >> m>> n;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin>>lo[i][j];
+        }
+    }
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cout<<lo[i][j];
+        }
+        cout<<endl;
+    }
+    return 0;
+}
