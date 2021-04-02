a624a8492ac43bb902dfc711ba2a11cab4998177
dlekgns6743 <dlekgns6743@naver.com>
2021-04-02 14:54:52 +0900

    bfs

--- class_3/7576.cpp
+++ class_3/7576.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int m,n;
     int lo[1001][1001];
-    cin >> m>> n;
+    cin>>m>>n;                  
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
             cin>>lo[i][j];
