1aa3b6f1fbc9ce30647c56e2ab2efdc7dff7dc93
dlekgns6743 <dlekgns6743@naver.com>
2021-04-09 21:49:24 +0900

    dp

--- class_4/11053.cpp
+++ class_4/11053.cpp
@@ -3,25 +3,29 @@ using namespace std;
 int arr[1001];
 int dp[1001];
 int mx=0;
+int ans;
 int main(){
+    ios::sync_with_stdio(1);
+	cin.tie(0);
     int n,tmp;
     cin>>n;
     for(int i=1; i<=n; i++){
         cin>>tmp;
         arr[i] = tmp;
+        dp[i] = 1;
     }
-    dp[1] = 1;
-    for(int i=2; i<=n; i++){
+    for(int i=1; i<=n; i++){
         int mx = 0;
-        for(int j=1; j<i; j++){
-            if(arr[j]<arr[i]){
-                if(mx<dp[j]){
-                    mx = dp[j];
-                }
+        for(int j=0; j<i; j++){
+            if(arr[j]<arr[i]&&mx<dp[j]){
+                mx = dp[j];
             }
         }
-    
+        dp[i] = mx + 1;
+        ans = max(dp[i],ans);
     }
-    cout<<mx;
+    cout<<ans<<" ";
 
 }
+
+// 이번 문제에서 나왔던 실수: 답으로 나와야 할 최대 길이는 배열 dp의 마지막에 있지만은 않다 = 전체에서 최대 길이를 출력해야함
