9c85ca206c728a551d0aefa1e9d2eb5c801b45f0
dlekgns6743 <dlekgns6743@naver.com>
2021-05-17 16:44:20 +0900

    210517

--- 바킹독 실전 알고리즘/3.배열 문제 및 코드/13300.cpp
+++ 바킹독 실전 알고리즘/3.배열 문제 및 코드/13300.cpp
@@ -1,11 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int r=0;
+    int tmp1, tmp2;
     int n,k;
-    int s,y;
+    int s[2][6]={0,};
     cin>>n>>k;
     for(int i=0; i<n; i++){
-        
+        cin>>tmp1>>tmp2;
+        s[tmp1][tmp2-1]++;
     }
+    for(int i=0; i<2; i++){
+        for(int j=0; j<6; j++){
+            r += s[i][j]/k;
+            if(s[i][j] % k != 0){
+                r++;
+            }
+        }
+    }
+    cout<<r;
 
 }
