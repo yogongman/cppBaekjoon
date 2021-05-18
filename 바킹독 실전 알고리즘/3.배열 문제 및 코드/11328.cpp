258891364b783026efd1144b090032f9738fa9b0
dlekgns6743 <dlekgns6743@naver.com>
2021-05-18 18:40:32 +0900

    210518

--- 바킹독 실전 알고리즘/3.배열 문제 및 코드/11328.cpp
+++ 바킹독 실전 알고리즘/3.배열 문제 및 코드/11328.cpp
@@ -9,9 +9,6 @@ int main(){
     string a,b;
     //97~122
     for(int i=0; i<N; i++){
-        answer=1;
-        fill(&ar[0],&ar[25],0);
-        fill(&br[0],&br[25],0);
         cin>>a>>b;
         for(auto ch: a){
             ar[ch-97]++;
@@ -21,13 +18,51 @@ int main(){
         }
         for(int i=0; i<26; i++){
             if(br[i]!=ar[i]){
-               answer--;
+               answer=0;
                break; 
             }
+            else answer=1;
         }
         if(answer) cout<<"Possible"<<"\n";
         else cout<<"Impossible"<<"\n";
+        fill(ar,ar+26,0);
+        fill(br,br+26,0);
     }
     return 0;
 
 }
+
+//  위에가 맞은 것
+
+// 밑에는 오답
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     int N,answer=1;
+//     int ar[26]={0,},br[26]={0,};
+//     cin>>N;
+//     string a,b;
+//     //97~122
+//     for(int i=0; i<N; i++){
+//         cin>>a>>b;
+//         for(auto ch: a){
+//             ar[ch-97]++;
+//         }
+//         for(auto ch: b){
+//             br[ch-97]++;
+//         }
+//         for(int i=0; i<26; i++){
+//             if(br[i]!=ar[i]){
+//                answer=0;
+//                break; 
+//             }
+//         }
+//         if(answer) cout<<"Possible"<<"\n";
+//         else cout<<"Impossible"<<"\n";
+//         fill(ar,ar+26,0);
+//         fill(br,br+26,0);
+//     }
+//     return 0;
+// }
