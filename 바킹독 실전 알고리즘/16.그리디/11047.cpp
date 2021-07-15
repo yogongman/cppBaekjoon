77abb9fe5d1ac32218b15d823ee72533537f3c8b
dlekgns6743 <dlekgns6743@naver.com>
2021-07-15 20:22:38 +0900

    210715

--- 바킹독 실전 알고리즘/16.그리디/11047.cpp
+++ 바킹독 실전 알고리즘/16.그리디/11047.cpp
@@ -1,5 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-in
-
+int N;
+int arr[11];
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int k;
+    cin>>N>>k;
+    int cnt=0;
+    for(int i=0; i<N; i++){
+        cin>>arr[i];
+    }
+    for(int i=N-1; i>=0; i--){
+        if(arr[i]>k) continue;
+        while(k>=0){
+            if(arr[i]>k) break;
+            k-=arr[i];
+            cnt++;
+        }
+    }
+    cout<<cnt;
 }
