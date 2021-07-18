b12473d3c874bdd284bcfb8c6d1202d1f7f7a12c
dlekgns6743 <dlekgns6743@naver.com>
2021-07-18 20:12:39 +0900

    210718

---
+++ 바킹독 실전 알고리즘/11.백트래킹/15664.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> arr(10);
+int a[10];
+bool isused[10];
+int n,m;
+void func(int k){
+    if(k==m){
+        for(int i=0; i<m; i++){
+          cout<<a[i]<<" ";  
+        }
+        cout<<"\n";
+        return;
+    }
+    int pre = -1;
+    // cout<<"\n"<<a[k-1]<<"P"<<"\n";
+    for(int i=0; i<n; i++){
+        if(!isused[i] && ( pre != arr[i]) && a[k-1]<=arr[i]){
+            pre = arr[i];
+            isused[i] = 1;
+            a[k]=arr[i];
+            func(k+1);
+            isused[i] = 0;
+        }
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    sort(arr.begin(),arr.begin()+n);
+    func(0);
+}
