9d6012e24bb5032d5199c0b63176199d4a4f0005
lee dahoon <yogongman@gmail.com>
2022-04-04 09:54:46 +0900

    220404

---
+++ basic/10990.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=n-i; j>0; j--){
+            cout<<" ";
+        }
+        for(int k=1; k<=i*2-1; k++){
+            if(k == 1|| i*2-1 == k){
+                cout<<"*";
+            }
+            else cout<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
