feb42dceaf9c71d0923d58704cd61641b94c0ed2
lee dahoon <yogongman@gmail.com>
2022-05-03 17:29:44 +0900

    220503

---
+++ dp/9252.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string a,b;
+    cin>>a>>b;
+
+    int arr[1001][1001];
+
+    for(int i=0; i<a.size(); i++){
+        for(int j=0; j<b.size(); j++){
+            
+        }
+    }
+}
